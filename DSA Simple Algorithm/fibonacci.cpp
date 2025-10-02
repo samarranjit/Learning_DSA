51e55d25f1e54282463a59ddc3d1cad688f843e6
Samar Ranjit <84336809+samarranjit@users.noreply.github.com>
2025-10-02 21:32:39 +0000

    DSA Simple Algorithms

--- DSA Simple Algorithm/fibonacci.cpp
+++ DSA Simple Algorithm/fibonacci.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
 
 using namespace std;
+
+
+int count =0;
+// Fibonacci first 18 terms
+void fibonacciRecursion(int term1, int term2){
+     if (count < 18){
+        int nextTerm = term1 + term2;
+         cout << nextTerm;
+        if (count != 17){
+            cout << ", ";}
+        count++;
+        fibonacciRecursion(term2, nextTerm);
+     }   
+    }
+
+// Finding the nth Fibonacci Term:
+int fibonacciNTerm(int N){
+    if (N <=1) {
+        return N;
+    }
+    else{
+        return fibonacciNTerm(N-1) + fibonacciNTerm(N-2);
+    }
+
+}
+
 int main(){
     cout << "Fibonacci Sequence up to 18 terms:" << endl;
 
@@ -17,8 +43,16 @@ int main(){
         term2 = nextTerm;
     }
 
+    // Implementation using Recursion:
+    cout << "\nFibonacci Sequence using Recursion:" << endl;
+    fibonacciRecursion(0, 1);
+    cout << endl;
+
+    // Printing the nth fibonacci term
+    cout << "Fibonacci Sequence for nth term: \n";
+    cout << fibonacciNTerm(19);
     
 
-
+    cout << endl;
     return 0;
 }
